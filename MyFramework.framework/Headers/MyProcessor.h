43eb16fbd76daa7bdb28df1759946a46ffaec039
Yogesh Bhople <yogeshbh@cybage.com>
2021-07-15 17:54:12 +0530

    updated framework

---
+++ MyFramework.framework/Headers/MyProcessor.h
@@ -0,0 +1,22 @@
+//
+//  Processor.h
+//  MyFramework
+//
+//  Created by Yogesh Bhople on 15/07/21.
+//
+
+#ifndef MyProcessor_h
+#define MyProcessor_h
+
+@interface MyProcessor: NSObject
+    
+    
+
+@property (nonatomic, retain) NSString* name;
+    
+- (void) printProcessorName;
+
+
+@end /* Processor_h */
+
+#endif
